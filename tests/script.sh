d8b4f8afc7433eff6e9662dbf36b37d619fc80d7
Siddhartha Jana <siddharthajana24@gmail.com>
2016-04-25 12:53:21 -0500

    Adding a version with finer grained locking

--- tests/script.sh
+++ tests/script.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 
-rm *.log 
+rm -rf *.log 
 
-for numcities in 04 14 15 16 17 20
+for numcities in 04 14 15 #16 17 20
 do
   for nprocs in 2 4 8 16 32 64 128 256 512
   do
-     for outfile in "tsp_shmem_PPQ" "tsp_shmem_MMPQ" "tsp_mpi"
+     for outfile in "tsp_shmem_LLPQ" # "tsp_shmem_PPQ" "tsp_shmem_MMPQ" "tsp_mpi"
      do
-        srun -p crill --exclusive -n $nprocs ${outfile}.out ./tspfile${numcities}.txt > temp.log
+        srun -p crill --exclusive -n $nprocs ${outfile}.out ../support/tspfile${numcities}.txt > temp.log
         grep "#Time;" ./temp.log >> ${outfile}.log
      done
   done
